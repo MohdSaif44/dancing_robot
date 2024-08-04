2d1bc47daae6495fe509df9a4e1b77658c17c04f
Mohamed Saif <mohamedali20@graduate.utm.my>
2024-08-04 21:31:00 +0800

    Added microros

--- Core/Src/MAIN/main.c
+++ Core/Src/MAIN/main.c
@@ -3,6 +3,11 @@
 #include "main.h"
 #include "cmsis_os.h"
 
+bool cubemx_transport_open(struct uxrCustomTransport * transport);
+bool cubemx_transport_close(struct uxrCustomTransport * transport);
+size_t cubemx_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+size_t cubemx_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
 /**
  * @brief  The application entry point.
  * @retval int
@@ -13,7 +18,7 @@ char msg[50];
 int main(void)
 {
 	set();
-//	HAL_NVIC_SystemReset();
+	//	HAL_NVIC_SystemReset();
 
 
 
@@ -36,6 +41,12 @@ int main(void)
 			.priority = (osPriority_t) osPriorityNormal,
 	};
 
+	const osThreadAttr_t Microros_Task_attributes = {
+			.name = "MicrorosTask",
+			.stack_size = 1024 * 10,
+			.priority = (osPriority_t) osPriorityNormal,
+	};
+
 	const osSemaphoreAttr_t CalcSemaphore_attributes = {
 			.name = "CalcSemaphore"
 	};
@@ -45,6 +56,8 @@ int main(void)
 	TaskOne_Task_Handle = osThreadNew(TaskOne, NULL, &TaskOne_Task_attributes);
 	TaskTwo_Task_Handle = osThreadNew(TaskTwo, NULL, &TaskTwo_Task_attributes);
 	TaskThree_Task_Handle = osThreadNew(TaskThree, NULL, &TaskThree_Task_attributes);
+	Micrors_Task_Handle = osThreadNew(MicrorosTask, NULL, &Microros_Task_attributes);
+
 	CalcSemaphore = osSemaphoreNew(1, 0, &CalcSemaphore);
 
 	osKernelStart();
@@ -60,11 +73,11 @@ void TaskOne(void *argument){
 
 	while(1){
 
-//		xr = (-ps4.joyL_x*cos(YawAngleAngle*3.14/180)-ps4.joyL_y*sin(YawAngleAngle*3.14/180));
-//		yr = (-ps4.joyL_x*sin(YawAngleAngle*3.14/180)+ps4.joyL_y*cos(YawAngleAngle*3.14/180));
+		//		xr = (-ps4.joyL_x*cos(YawAngleAngle*3.14/180)-ps4.joyL_y*sin(YawAngleAngle*3.14/180));
+		//		yr = (-ps4.joyL_x*sin(YawAngleAngle*3.14/180)+ps4.joyL_y*cos(YawAngleAngle*3.14/180));
 
-//		xr = -ps4.joyL_x;
-//		yr =  ps4.joyL_y;
+		//		xr = -ps4.joyL_x;
+		//		yr =  ps4.joyL_y;
 
 		if (ps4.button & UP) {
 
@@ -81,14 +94,14 @@ void TaskOne(void *argument){
 
 		}
 
-//		else if  (ps4.button & DOWN){
-//
-//			RNSVelocity(-0.75,-0.75,-0.75,-0.75,&rns);
-//		}
-//		else{
-//
-//			RNSStop(&rns);
-//		}
+		//		else if  (ps4.button & DOWN){
+		//
+		//			RNSVelocity(-0.75,-0.75,-0.75,-0.75,&rns);
+		//		}
+		//		else{
+		//
+		//			RNSStop(&rns);
+		//		}
 		xr = -1.5*(ps4.joyL_x);
 		yr = 1.5*(ps4.joyL_y);
 		wr=(ps4.joyR_2-ps4.joyL_2);
@@ -96,30 +109,30 @@ void TaskOne(void *argument){
 		sprintf(msg, "%f  %f \n", ps4.joyL_x, ps4.joyL_y);
 		UARTPrintString(&huart2, msg);
 
-//		if(ps4.joyL_x || ps4.joyL_y){
-//
-////			xr = -1 * ((-ps4.joyL_x * cos((YawAngle + 180) * 3.14 / 180)) - (ps4.joyL_y * sin((YawAngle + 180) * 3.14 / 180)));
-////			yr = -1 * (( ps4.joyL_y * cos((YawAngle + 180) * 3.14 / 180)) - (ps4.joyL_x * sin((YawAngle + 180) * 3.14 / 180)));
-//		}
-//		else{
-//			RNSStop(&rns);
-//		}
+		//		if(ps4.joyL_x || ps4.joyL_y){
+		//
+		////			xr = -1 * ((-ps4.joyL_x * cos((YawAngle + 180) * 3.14 / 180)) - (ps4.joyL_y * sin((YawAngle + 180) * 3.14 / 180)));
+		////			yr = -1 * (( ps4.joyL_y * cos((YawAngle + 180) * 3.14 / 180)) - (ps4.joyL_x * sin((YawAngle + 180) * 3.14 / 180)));
+		//		}
+		//		else{
+		//			RNSStop(&rns);
+		//		}
 
 		MODN(&Modn);
 
-//		if(fabs(v1) + fabs(v2) + fabs(v3) + fabs(v4) > 0.05){
-//
-			RNSVelocity(v1, v2, v3, v4, &rns);
-//
-//		}else{
-//
-//			RNSStop(&rns);
-//		}
+		//		if(fabs(v1) + fabs(v2) + fabs(v3) + fabs(v4) > 0.05){
+		//
+		RNSVelocity(v1, v2, v3, v4, &rns);
+		//
+		//		}else{
+		//
+		//			RNSStop(&rns);
+		//		}
 
 
 		osDelay(1);
 
-		}
+	}
 
 }
 
@@ -146,10 +159,70 @@ void TaskThree(void *argument){
 
 }
 
+void MicrorosTask(void *argument){
+
+	// Get empty allocator
+	rcl_allocator_t custom_allocator = rcutils_get_zero_initialized_allocator();
+
+	// Set custom allocation methods
+	custom_allocator.allocate = microros_allocate;
+	custom_allocator.deallocate = microros_deallocate;
+	custom_allocator.reallocate = microros_reallocate;
+	custom_allocator.zero_allocate =  microros_zero_allocate;
+
+	rcutils_set_default_allocator(&custom_allocator);
+
+	// Initialize allocator object
+	allocator = rcutils_get_zero_initialized_allocator();
+	// Initialize support object
+	rclc_support_init(&support, 0, NULL, &allocator);
+	// Init default node
+	rclc_node_init_default(&node, "node", "", &support);
+	// Init default executor
+	executor = rclc_executor_get_zero_initialized_executor();
+	rclc_executor_init(&executor, &support.context, 1, &allocator);
+
+
+	rmw_uros_set_custom_transport(
+			true,
+			(void *) &hpcd_USB_OTG_FS,
+			cubemx_transport_open,
+			cubemx_transport_close,
+			cubemx_transport_write,
+			cubemx_transport_read
+	);
+
+	/******* Subscriper *******/
+	rclc_subscription_init_default(
+			&coor_subscriber,
+			&node,
+			ROSIDL_GET_MSG_TYPE_SUPPORT(turtle_bot_msgs, msg, Coordinates),
+			"coor_callback");
+
+	rclc_executor_add_subscription(
+			&executor,
+			&coor_subscriber,
+			&coor_msg,
+			&coor_callback,
+			ON_NEW_DATA);
+
+	while(1){
+
+		rclc_executor_spin_some(&executor, 50 * 1000000); // 50ms
+
+		osDelay(1);
+	}
+
+}
+
+void coor_callback(const void * msgin){
+
+}
+
 void TIM7_IRQHandler(void) { //5ms
 
 	osSemaphoreRelease(CalcSemaphore);
-//	PSxConnectionHandler(&ps4);
+	//	PSxConnectionHandler(&ps4);
 	HAL_TIM_IRQHandler(&htim7);
 }
 
--- Core/Src/MAIN/main.h
+++ Core/Src/MAIN/main.h
@@ -9,14 +9,43 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "../common.h"
 
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <uxr/client/transport.h>
+#include <rmw_microxrcedds_c/config.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include "turtle_bot_msgs/msg/coordinates.h"
+
 void TaskOne(void *argument);
 void TaskTwo(void *argument);
 void TaskThree(void *argument);
+void MicrorosTask(void *argument);
+
+
+void * microros_allocate(size_t size, void * state);
+void microros_deallocate(void * pointer, void * state);
+void * microros_reallocate(void * pointer, size_t size, void * state);
+void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
+
+void coor_callback(const void * msgin);
 
 osThreadId_t TaskOne_Task_Handle;
 osThreadId_t TaskTwo_Task_Handle;
 osThreadId_t TaskThree_Task_Handle;
+osThreadId_t Micrors_Task_Handle;
 osSemaphoreId_t CalcSemaphore;
+
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rclc_executor_t executor;
+
+rcl_subscription_t coor_subscriber;
+
+turtle_bot_msgs__msg__Coordinates coor_msg;
 
 
 #ifdef __cplusplus
